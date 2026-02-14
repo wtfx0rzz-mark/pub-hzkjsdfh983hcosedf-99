de8cd08fdcc88fc93de2c42f0f12b982d02d93df
wtfx0rzz-mark <wtfmark@pm.me>
2026-02-14 16:49:56 -0600

    Fix URL in loadstring for stage.lua

--- stage.lua
+++ stage.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfx0rzz-mark//pub-hzkjsdfh983hcosedf-99/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfx0rzz-mark/pub-hzkjsdfh983hcosedf-99/main.lua"))()
