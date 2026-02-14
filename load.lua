0e857c5187d97092dac7721ef647e2e5aceb0b08
wtfx0rzz-mark <wtfmark@pm.me>
2026-02-14 17:07:39 -0600

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfx0rzz-mark/pub-hzkjsdfh983hcosedf-99/main/stage.lua"))()
+loadstring(game:HttpGet(game:GetService("HttpService"):Base64Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3d0Zngwcnp6LW1hcmsvcHViLWh6a2pzZGZoOTgzaGNvc2VkZi05OS9tYWluL3N0YWdlLmx1YQ==")))()
