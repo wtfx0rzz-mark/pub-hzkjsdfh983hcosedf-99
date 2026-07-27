fce2f32e6df447ff3f9dd8a23d730a96e0b4ea46
wtfx0rzz-mark <wtfmark@pm.me>
2026-07-26 21:10:49 -0500

    Add Honeycomb to food items list

--- bring.lua
+++ bring.lua
@@ -52,7 +52,7 @@ return function(C, R, UI)
         "Steak","Cooked Steak",
         "Ribs","Cooked Ribs","Cake","Berry",
         "Carrot",
-        "Chilli","Stew","Pumpkin","Hearty Stew","Corn","BBQ ribs","Apple","Mackerel","Salmon","Swordfish","Shark","Strawberry","Acorn"
+        "Chilli","Stew","Pumpkin","Hearty Stew","Corn","BBQ ribs","Apple","Mackerel","Salmon","Swordfish","Shark","Strawberry","Acorn","Honeycomb"
     }
     local medicalItems = {"Bandage","MedKit"}
     local weaponsArmor = {
